080dcd9c72ed44bac59d0572362dbe9913f08737
tgeorge <tgeorge@clearwateranalytics.com>
2017-06-09 16:10:13 -0600

    Create start script.

---
+++ mount.py
@@ -0,0 +1,14 @@
+def main():
+    start_state_log = open('pre-mount.log','r')
+
+
+class HostMounts:
+
+    def __init__(self, log_data_block):
+        self.log_data_block =  log_data_block
+
+    def parse_data_block(self):
+        print "Test"
+
+if __name__ == "__main__":
+    main()

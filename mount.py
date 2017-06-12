e44df9c3a4fb0eedfcb26429eb7c10581737ed40
tgeorge <ritzz32@gmail.com>
2017-06-11 21:41:15 -0600

    First functions

--- mount.py
+++ mount.py
@@ -1,5 +1,33 @@
+import re
+
+
 def main():
-    start_state_log = open('pre-mount.log','r')
+    start_state_log = open('pre-mount.log', 'r')
+
+    parse_state_log(start_state_log)
+
+
+def parse_state_log(log_file):
+    host = False
+
+    for line in log_file:
+
+        re_section_match = '#{34}'
+        re_host_match = '[a-zA-Z0-9]*-[a-zA-Z0-9]*-[a-zA-Z0-9]*-[a-zA-Z0-9]*-[a-zA-Z]*[0-9]*'
+        re_fstab_match = '^.*cifs.*'
+        re_symlink_match = '^lrwxrwxrwx.*->.*'
+
+        if re.search(re_section_match, line):
+            pass
+
+        elif re.search(re_host_match, line):
+            host = line
+
+        elif re.search(re_fstab_match, line):
+            print('{} {}'.format(host, line))
+
+        elif re.search(re_symlink_match, line):
+            print('{} {}'.format(host, line))
 
 
 class HostMounts:
@@ -8,7 +36,7 @@ class HostMounts:
         self.log_data_block =  log_data_block
 
     def parse_data_block(self):
-        print "Test"
+        print("Test")
 
 if __name__ == "__main__":
     main()

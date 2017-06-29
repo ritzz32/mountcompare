7fff8ff73ed771419c267840994526bb1baa2034
tgeorge <tgeorge@clearwateranalytics.com>
2017-06-29 10:58:41 -0600

    Update gitignore

--- mount.py
+++ mount.py
@@ -4,8 +4,8 @@ from pprint import pprint
 
 
 def main():
-    start_state_log = open('st-pre-mount.log', 'r')
-    end_state_log = open('st-post-mount.log', 'r')
+    start_state_log = open('prod-pre-mount.log', 'r')
+    end_state_log = open('prod-post-mount.log', 'r')
     host_mounts = {}
 
     parse_state_log(start_state_log, host_mounts, 'pre')
